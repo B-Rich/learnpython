b5f84d663ca0991325b99d22ede737a46c8026f8
Shuwen <chris.void.92@gmail.com>
2015-03-08 21:15:23 +0800

    update

---
+++ lambda.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+def add(x,y):
+    return x+y ? lambda x,y : x+y
+
+def usuallyAdd2(x,y=2):
+    return x+y ? lambda x,y=2 : x+y
+
+def showAllAsTuple(*z):
+    return z ? lambda *z : z
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# Ex 11.5
+
+def testit(func, *nkwargs, **kwargs):
+    try:
+        retval = func(*nkwargs, **kwargs)
+        result = (True, retval)
+    except Exception, diag:
+        result = (False, str(diag))
+    return result
+
+
+def test():
+    func 
